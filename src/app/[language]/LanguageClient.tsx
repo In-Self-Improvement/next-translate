e8c86fee6f02e7cd388b74d31b098cab6a71796d
Bong <borobong230@gmail.com>
2024-02-29 18:19:25 +0900

    :sparkles: [Feat] Language 기본 뼈대 생성, package 추가 #main

---
+++ src/app/[language]/LanguageClient.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import classnames from "classnames/bind";
+import styles from "./LanguageClient.module.scss";
+
+interface LanguageClientProps {}
+
+const LanguageClient: React.FC<LanguageClientProps> = () => {
+  return <div> LanguageClient </div>;
+};
+
+export default LanguageClient;
---
+++ src/app/[language]/page.tsx
@@ -0,0 +1,7 @@
+import LanguageClient from "./LanguageClient";
+
+const Language = () => {
+  return <LanguageClient />;
+};
+
+export default Language;
